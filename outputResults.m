2f85835982dc7210ad53487ef4ba2c3879f4cd32
bofo90 <f.agustin@gmail.com>
2017-08-03 10:36:04 +0200

    output with hinges name

--- outputResults.m
+++ outputResults.m
@@ -23,7 +23,7 @@ opt.tranPol=0.5;
 %PREPARE PLOTTING UNDEFORMED CONFIGURATION
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Check if output folder is required, and create it if it doesn't exist
- nameFolder=[pwd,'/Results/',opt.template];
+ nameFolder=[pwd,'/Results/',opt.template,'/',opt.relAlgor];
 if or(strcmp(opt.saveFig,'on'),strcmp(opt.saveMovie,'on'))
     if exist(nameFolder)==0
         mkdir(nameFolder)
@@ -325,10 +325,19 @@ if strcmp(opt.plot,'result')
                         set(hs{nc,i},'Vertices',plotextrudedUnitCell.mode(nMode).frame(framMode).lat(nc).coor,'facecolor','flat','facevertexCData',c*colt(4,:)+abs(1-c)*colt(5,:),'facealpha',1.0);
                     end
                 end
-
+%                 if isempty(opt.angleConstrFinal(nMode))
+%                     printGif(opt,framMode,f,nameFolder,['_[',num2str(opt.angleConstrFinal(nMode-1).val(:,1)'),']_',num2str(nMode),'_deformed']);
+%                 else
+%                     printGif(opt,framMode,f,nameFolder,['_[',num2str(opt.angleConstrFinal(nMode).val(:,1)'),']_',num2str(nMode),'_deformed']);
+%                 end
                 printGif(opt,framMode,f,nameFolder,[num2str(nMode),'_deformed']);
                 if framMode==length(plotextrudedUnitCell.mode(nMode).frame)
-                    printHigRes(f,opt,[num2str(nMode),'_deformed'],nameFolder)     
+                    printHigRes(f,opt,[num2str(nMode),'_deformed'],nameFolder)
+%                     if isempty(opt.angleConstrFinal(nMode))
+%                         printHigRes(f,opt,['_[',num2str(opt.angleConstrFinal(nMode-1).val(:,1)'),']_',num2str(nMode),'_deformed'],nameFolder)     
+%                     else
+%                         printHigRes(f,opt,['_[',num2str(opt.angleConstrFinal(nMode).val(:,1)'),']_',num2str(nMode),'_deformed'],nameFolder)
+%                     end
                 end
             end
             
@@ -437,7 +446,7 @@ function hl2=plotOpt(opt)
 function printGif(opt,fram,f,nameFolder,nam)
     pause(1/opt.frames)
     if strcmp(opt.saveMovie,'on')
-        name=[nameFolder,'/',opt.template,'_', nam];
+        name=[nameFolder,'/',opt.template,'_[',num2str(opt.angleConstrFinal(1).val(:,1)'),']_', nam];
         switch opt.plot
             case 'modes'
                 name=[name,'_Modes_'];
@@ -472,7 +481,7 @@ function printHigRes(f,opt,nam,nameFolder)
     switch opt.saveFig
         case 'on'
 %             name=[nameFolder,'/',opt.template,'_',num2str(opt.plotPer),'_',nam];
-            name=[nameFolder,'/',opt.template,'_',nam];
+            name=[nameFolder,'/',opt.template,'_[',num2str(opt.angleConstrFinal(1).val(:,1)'),']_',nam];
             figpos=getpixelposition(f); %dont need to change anything here
             resolution=get(0,'ScreenPixelsPerInch'); %dont need to change anything here
             set(f,'paperunits','inches','papersize',figpos(3:4)/resolution,...
