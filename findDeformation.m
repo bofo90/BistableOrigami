4f9db3e787c5bf3fbc7edf32f4f2c0e9bb60de2f
bofo90 <f.agustin@gmail.com>
2018-06-21 17:54:21 +0200

    run for rhombic dodecahedron

--- findDeformation.m
+++ findDeformation.m
@@ -21,7 +21,7 @@ if strcmp(opt.analysis,'result')
                 minHinge = opt.minHinges;
                 numHinges = size(hingeList, 1);
                 anglFold = (-pi*(opt.constAnglePerc-0.005));
-                parfor i = 1:numHinges
+                for i = 1:numHinges
                     row = hingeList(i, :);
                     hinges = row(0~=row);
                     if length(hinges) <= maxHinge && length(hinges) >= minHinge
