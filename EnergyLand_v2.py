de127f5694951231ee8ef01601c71f372b5282cc
bofo90 <f.agustin@gmail.com>
2019-05-22 17:28:29 +0200

    add stable state analysis

--- EnergyLand_v2.py
+++ EnergyLand_v2.py
@@ -10,6 +10,22 @@ import numpy as np
 import configparser
 import os.path
 
+def countStableStates(angles, steps, simulations):
+    
+    import scipy.cluster.hierarchy as hierarch
+    from scipy.spatial.distance import pdist
+    
+    finalAngles = np.empty((0,np.size(dataAngles,1)))
+    for hinge in np.arange(simulations):
+        sortAllAngIndex = np.lexsort((angles[steps*(hinge+1)-1,:],angles[steps*hinge,:]))
+        finalAngles = np.append(finalAngles, [dataAngles[steps*(hinge+1)-1,sortAllAngIndex]], axis = 0)
+                                                         
+    Z = hierarch.linkage(finalAngles, 'centroid')
+    inverse = hierarch.fcluster(Z, 1, criterion='distance')
+    c = hierarch.cophenet(Z, pdist(finalAngles))
+    print('this is the cophenet of the hierarchical linkage', c[0])
+    
+    return inverse
 folder_name = "Results/SingleVertex3/active-set/energy/21-May-2019_temp/kh0.000_kta100.000_ke1.000_kf100.000"
 
 file_name1 = "/EnergyData.csv" 
@@ -34,6 +50,8 @@ flagmask = (exfl !=1) & (exfl !=5) & (exfl !=5)
 flagmask = ~flagmask.any(axis= 1)
 
 dataVar['Mask'] = flagmask
+dataVar['StableStates'] = countStableStates(dataAngles, IterPerSimulAngles, TotSimul)
+
 dataVar = dataVar.join(dataEnergy[['Hinge Number','TotalEnergy']][IterPerSimulEnergy-2::IterPerSimulEnergy].set_index('Hinge Number'), on = 'HingeNumber')
 dataVar.set_index(['Kappa','TargetAngle'], inplace = True)
 dataVar.sort_index(inplace=True)
