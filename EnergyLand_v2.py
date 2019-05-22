dadee852e3be6c3a95e80f2161b0045cd05c0205
bofo90 <f.agustin@gmail.com>
2019-05-22 17:28:38 +0200

    try to plot

--- EnergyLand_v2.py
+++ EnergyLand_v2.py
@@ -7,8 +7,14 @@ Created on Tue May 21 18:04:51 2019
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as matl
+import xarray as xr
 import configparser
 import os.path
+
+def cm2inch(value):
+    return value/2.54
 
 def countStableStates(angles, steps, simulations):
     
@@ -26,6 +32,42 @@ def countStableStates(angles, steps, simulations):
     print('this is the cophenet of the hierarchical linkage', c[0])
     
     return inverse
+
+def NiceGraph2D(axes, nameX, nameY, mincoord = [np.NaN, np.NaN], maxcoord = [np.NaN, np.NaN], divisions = [np.NaN, np.NaN],
+                buffer = [0.0, 0.0, 0.0]):
+    gray = '0.2'
+    matl.rcParams.update({'font.size': 11})
+
+    if ~np.isnan(mincoord[0]) and ~np.isnan(maxcoord[0]):
+        axes.set_xlim([mincoord[0]-buffer[0], maxcoord[0]+buffer[0]])
+        if isinstance(divisions[0], (list, tuple, np.ndarray)):
+            if ~np.isnan(divisions[0]).any():
+                axes.set_xticks(divisions[0])
+        else:
+            if ~np.isnan(divisions[0]):
+                axes.set_xticks(np.linspace(mincoord[0],maxcoord[0],divisions[0]))
+                            
+    axes.set_xlabel(nameX)
+    if ~np.isnan(mincoord[1]) and ~np.isnan(maxcoord[1]):
+        axes.set_ylim([mincoord[1]-buffer[1], maxcoord[1]+buffer[1]])
+        if isinstance(divisions[1], (list, tuple, np.ndarray)):
+            if ~np.isnan(divisions[1]).any():
+                axes.set_yticks(divisions[1])
+        else:
+            if ~np.isnan(divisions[1]):
+                axes.set_yticks(np.linspace(mincoord[1],maxcoord[1],divisions[1]))
+    axes.set_ylabel(nameY)
+   
+    axes.xaxis.label.set_color(gray)
+    axes.tick_params(axis='x',colors=gray)
+    axes.spines['bottom'].set_color(gray)
+    axes.spines['top'].set_color(gray)
+    axes.yaxis.label.set_color(gray)
+    axes.tick_params(axis='y', colors=gray)
+    axes.spines['left'].set_color(gray)
+    axes.spines['right'].set_color(gray)
+    return
+
 folder_name = "Results/SingleVertex3/active-set/energy/21-May-2019_temp/kh0.000_kta100.000_ke1.000_kf100.000"
 
 file_name1 = "/EnergyData.csv" 
@@ -43,6 +85,7 @@ dataAngles = np.delete(dataAngles, 0, 1)
 
 TotSimul = dataVar.shape[0]
 IterPerSimulEnergy = np.int(dataEnergy.shape[0]/TotSimul)
+IterPerSimulAngles = np.int(dataAngles.shape[0]/TotSimul)
 
 print(dataEnergy['Flags'].value_counts())
 exfl = dataEnergy.Flags.values.reshape(TotSimul,IterPerSimulEnergy)
@@ -56,6 +99,22 @@ dataVar = dataVar.join(dataEnergy[['Hinge Number','TotalEnergy']][IterPerSimulEn
 dataVar.set_index(['Kappa','TargetAngle'], inplace = True)
 dataVar.sort_index(inplace=True)
 
-dataEnergy.set_index('Hinge Number', inplace = True)
-dataEnergy.sort_index(inplace=True)
+data = dataVar.to_xarray()
 
+fig1 = plt.figure(0,figsize=(cm2inch(24.1), cm2inch(20)))
+ax1 = plt.subplot(111)
+
+data['TotalEnergy'].plot(axes = ax1, cmap = matl.cm.nipy_spectral, cbar_kwargs={'ticks': np.linspace(0, data['TotalEnergy'].max(), 5),
+    'pad': 0.01})
+    
+NiceGraph2D(ax1, 'a', 'b')
+
+ax1.xaxis.set_major_formatter(matl.ticker.FormatStrFormatter('%.2g'))
+ax1.yaxis.set_major_formatter(matl.ticker.FormatStrFormatter('%.2g'))
+
+#cbar = plt.colorbar(cs1,  ax = ax1, fraction=0.05, pad=0.01, extend = 'max')#format="%d", 
+#cbar.set_ticks(np.linspace(0, maxEnergy, 5))
+#cbar.set_label('Energy', fontsize = 11, color = '0.2')
+#cbar.ax.tick_params(axis='y',colors='0.2')
+#cbar.ax.tick_params(axis='x',colors='0.2')
+#cbar.outline.set_edgecolor('0.2')
