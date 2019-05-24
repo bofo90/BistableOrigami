ed13e6c935c724576755743b3a3f006d591d4284
bofo90 <f.agustin@gmail.com>
2019-05-24 15:57:25 +0200

    create graph of energy against target anglge wqith colormap of kappa

--- EnergyLand_v2.py
+++ EnergyLand_v2.py
@@ -66,7 +66,7 @@ def NiceGraph2D(axes, nameX, nameY, mincoord = [np.NaN, np.NaN], maxcoord = [np.
                             
     axes.set_xlabel(nameX)
     if ~np.isnan(mincoord[1]) and ~np.isnan(maxcoord[1]):
-        axes.set_ylim([mincoord[1]*(-buffer[1]), maxcoord[1]*(buffer[1])])
+        axes.set_ylim([mincoord[1]-buffer[1], maxcoord[1]+buffer[1]])
         if isinstance(divisions[1], (list, tuple, np.ndarray)):
             if ~np.isnan(divisions[1]).any():
                 axes.set_yticks(divisions[1])
@@ -85,7 +85,44 @@ def NiceGraph2D(axes, nameX, nameY, mincoord = [np.NaN, np.NaN], maxcoord = [np.
     axes.spines['right'].set_color(gray)
     return
 
-folder_name = "Results/SingleVertex3/sqp/energy/23-May-2019_angle_kappa/kh0.000_kta100.000_ke1.000_kf100.000"
+def NiceGraph2Dlog(axes, nameX, nameY, mincoord = [np.NaN, np.NaN], maxcoord = [np.NaN, np.NaN], divisions = [np.NaN, np.NaN],
+                buffer = [0.0, 0.0, 0.0]):
+    gray = '0.2'
+    matl.rcParams.update({'font.size': 11})
+
+    if ~np.isnan(mincoord[0]) and ~np.isnan(maxcoord[0]):
+        axes.set_xlim([mincoord[0]-buffer[0], maxcoord[0]+buffer[0]])
+        if isinstance(divisions[0], (list, tuple, np.ndarray)):
+            if ~np.isnan(divisions[0]).any():
+                axes.set_xticks(divisions[0])
+        else:
+            if ~np.isnan(divisions[0]):
+                axes.set_xticks(np.linspace(mincoord[0],maxcoord[0],divisions[0]))
+                            
+    axes.set_xlabel(nameX)
+    if ~np.isnan(mincoord[1]) and ~np.isnan(maxcoord[1]):
+        axes.set_ylim([mincoord[1]*((buffer[1])**(-1)), maxcoord[1]*(buffer[1])])
+        if isinstance(divisions[1], (list, tuple, np.ndarray)):
+            if ~np.isnan(divisions[1]).any():
+                axes.set_yticks(divisions[1])
+        else:
+            if ~np.isnan(divisions[1]):
+                axes.set_yticks(np.logspace(np.log10(mincoord[1]),np.log10(maxcoord[1]),divisions[1]))
+    axes.set_ylabel(nameY)
+   
+    axes.xaxis.label.set_color(gray)
+    axes.tick_params(axis='x',colors=gray)
+    axes.spines['bottom'].set_color(gray)
+    axes.spines['top'].set_color(gray)
+    axes.yaxis.label.set_color(gray)
+    axes.tick_params(axis='y', colors=gray)
+    axes.spines['left'].set_color(gray)
+    axes.spines['right'].set_color(gray)
+    return
+
+plt.close('all')
+
+folder_name = "Results/SingleVertex3/sqp/energy/24-May-2019_angle_kappa_nomaxStretch_allHing/kh0.000_kta100.000_ke1.000_kf100.000"
 
 file_name1 = "/EnergyData.csv" 
 file_name2 = "/Hinges.csv"
@@ -122,18 +159,31 @@ data = dataVar.to_xarray()
 
 fig1 = plt.figure(0,figsize=(cm2inch(24.1), cm2inch(20)))
 ax1 = plt.subplot(111)
+fig1.subplots_adjust(top=0.99,
+bottom=0.115,
+left=0.130,
+right=0.89)
 
-data['TotalEnergy'].where(data.Mask).plot(axes = ax1, cmap = matl.cm.nipy_spectral, 
-    cbar_kwargs={'ticks': np.linspace(data['TotalEnergy'].min(), data['TotalEnergy'].max(), 5),
-    'pad': 0.01, 'format':r"$10^{%.2f}$"})
+norm = matl.colors.LogNorm(vmin = data.Kappa.min(),vmax = data.Kappa.max())
+cmap = matl.cm.ScalarMappable(norm = norm, cmap=matl.cm.nipy_spectral)
+cmap.set_array([])
+
+for i in np.arange(np.size(data.Kappa.data)):
+    cs1 = data['TotalEnergy'].where(data.Mask)[data.Kappa == data.Kappa.data[i]].plot(axes = ax1, c = cmap.to_rgba(data.Kappa.data[i]))
+    #, cmap = matl.cm.nipy_spectral, 
+    #    cbar_kwargs={'ticks': np.linspace(data['TotalEnergy'].min(), data['TotalEnergy'].max(), 5),
+    #    'pad': 0.01, 'format':r"$10^{%.2f}$"})
     
-NiceGraph2D(ax1, 'Target Angle', 'Kappa', mincoord = [-1,0.0001], maxcoord = [1,1],
-            divisions = [5, 5], buffer = [0.1, 10**(0.5)])
-ax1.set_yscale('log')
+plt.title('')
 
-#cbar = plt.colorbar(cs1,  ax = ax1, fraction=0.05, pad=0.01, extend = 'max')#format="%d", 
-#cbar.set_ticks(np.linspace(0, maxEnergy, 5))
-#cbar.set_label('Energy', fontsize = 11, color = '0.2')
+NiceGraph2D(ax1, 'Target Angle', 'log(Energy)', mincoord = [-1,-5], maxcoord = [1,2],
+            divisions = [5, 8], buffer = [0.01, 0.1])
+
+
+
+cbar = fig1.colorbar(cmap, ticks = np.logspace(-4,0,5), fraction=0.05, pad=0.01) 
+#cbar.set_ticks(np.linspace(-3, 0, 5))
+#cbar.set_label('Kappa', fontsize = 11, color = '0.2')
 #cbar.ax.tick_params(axis='y',colors='0.2')
 #cbar.ax.tick_params(axis='x',colors='0.2')
 #cbar.outline.set_edgecolor('0.2')
