fe782d286f76ca649b6a8d6f259c4e1bc7fd82c3
bofo90 <f.agustin@gmail.com>
2019-11-29 15:54:03 +0100

    adjust the plots

--- EnergyLand_v2.py
+++ EnergyLand_v2.py
@@ -467,8 +467,8 @@ lines = ['-','--',':']#,'-.'
 cmap = matl.cm.get_cmap('Set2',np.size(stst))
 colors = cmap(np.linspace(0,1,np.size(stst)))
 
-yticks = [[2,1,0,-1,-2],[10,0,-10,-20,-30],[1,0,-1,-2,-3]]
-ylim = [[2.1,-2.1],[12,-39],[1.1,-3.1]]
+yticks = [[-1.2,-0.6,0,0.6,1.2],[-36,-24,-12,0,12],[-2.4,-1.6,-0.8,0]]
+ylim = [[-1.5,1.5],[-39,13],[-2.7,0.2]]
 
 
 for i in np.arange(np.size(restangles)):
@@ -476,8 +476,8 @@ for i in np.arange(np.size(restangles)):
     fig1 = plt.figure(figsize=(cm2inch(5.5), cm2inch(4)))
     ax1 = plt.subplot(111)
     fig1.subplots_adjust(top=0.962,
-    bottom=0.27,
-    left=0.24,
+    bottom=0.25,
+    left=0.22,
     right=0.987)
     
     thisangBool = allDesigns['restang'] == restangles[i]
@@ -487,28 +487,25 @@ for i in np.arange(np.size(restangles)):
     maxCurv = np.ceil(thisang['Curvature'].max())#quantile(0.95)
     minCurv = np.floor(thisang['Curvature'].min())
     
-        #    NiceGraph2D(ax1,  r'$Log(\kappa)$', r'$E_\mathrm{tot}$', mincoord=[np.log10(kappas[0]),0], maxcoord=[np.log10(kappas[-1]),maxTotEn], divisions=[5, 3], buffer=[0.1, 0.01])
-    #NiceGraph2D(ax1, 'Kappa', 'Hinge Energy', mincoord=[kappas[0],0], maxcoord=[kappas[-1],maxHinEn], divisions=[5, 3], buffer=[0, 0.001])
 #    NiceGraph2D(ax1, r'$\kappa$', r'$E_\mathrm{tot}$')
     NiceGraph2D(ax1, r'$\kappa$', r'$C$')#, mincoord=[np.log10(kappas[0]),-3], maxcoord=[np.log10(kappas[-1]),3], divisions=[5, 5], buffer=[0.1, 1])
     
-    ax1.set_yticks(np.linspace(0, maxTotEn,5))
-    ax1.set_ylim([-0.005, maxTotEn+0.005])
-    ax1.yaxis.set_major_formatter(matl.ticker.FormatStrFormatter('%.2f'))
+#    ax1.set_yticks(np.linspace(0, maxTotEn,5))
+#    ax1.set_ylim([-0.005, maxTotEn+0.005])
+#    ax1.yaxis.set_major_formatter(matl.ticker.FormatStrFormatter('%.2f'))
     ax1.set_yticks(yticks[i])
     ax1.set_ylim(ylim[i])
 #    ax1.yaxis.set_major_formatter(matl.ticker.FormatStrFormatter('%.1f'))
     ax1.set_xscale('log')
+    ax1.set_xticks([0.001,0.01,0.1,1,10])
     ax1.set_xlim([0.0007,15])
         
     for j in stst:
         thisstst = thisang[thisang.StableStateAll == j]
 #    plt.close('all')
 
-#    ax1.scatter(np.log10(thisang['kappa']), thisang['TotalEnergy'], c = colors[thisang['StableStateAll']-1])
-#    ax1.scatter(thisang['kappa'], thisang['EdgeEnergy'], c = colors[thisang['StableStateAll']-1])
 #        ax1.scatter(thisstst['kappa'], thisstst['TotalEnergy'], c = colors[thisstst['StableStateAll'].values.astype('int')-1], s = 10)
-        ax1.scatter(thisstst['kappa'], thisstst['Curvature'], c = colors[thisstst['StableStateAll'].values.astype('int')-1], s = 10)#,
+        ax1.scatter(thisstst['kappa'], thisstst['Curvature'], c = colors[thisstst['StableStateAll'].values.astype('int')-1], s = 5)#,
 #                s = 5, marker = markers[i])#, linestyle = lines[i], lw = 2.5)
 
 #leg = ax1.legend(loc = 2, fontsize = 7, framealpha = 0.8, edgecolor = 'inherit', fancybox = False) 
@@ -519,8 +516,8 @@ for i in np.arange(np.size(restangles)):
     fig1.show()
 #    fig1.savefig(Folder_name + '/Images/Energy_Restang_' + str(i.astype(float))+'.pdf', transparent = True)
 #    fig1.savefig(Folder_name + '/Images/Energy_Restang_' + str(i.astype(float))+ '.png', transparent = True)
-#    fig1.savefig(Folder_name + '/Images/Restang_' + str(restangles[i].astype(float))+'.pdf', transparent = True)
-#    fig1.savefig(Folder_name + '/Images/Restang_' + str(restangles[i].astype(float))+ '.png', transparent = True)
+    fig1.savefig(Folder_name + '/Images/Restang_' + str(restangles[i].astype(float))+'.pdf', transparent = True)
+    fig1.savefig(Folder_name + '/Images/Restang_' + str(restangles[i].astype(float))+ '.png', transparent = True)
 
 #%%
 #fig3 = plt.figure(figsize=(cm2inch(8), cm2inch(6)))
